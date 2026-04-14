8cf5a19c5a7570fc561ea28ba6bdb52a026033ad
Kris Hsu <210645322+ruby23719111@users.noreply.github.com>
2026-04-15 03:41:29 +1000

    Initial Commit

---
+++ JuniorRangerApp/JuniorRangerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JuniorRangerAppApp.swift
+//  JuniorRangerApp
+//
+//  Created by Kris Hsu on 15/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct JuniorRangerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
